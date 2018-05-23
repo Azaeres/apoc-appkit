cdeed71ff1afbf0df31bf7d23ecdc42ecbfc7ef6
Ryan Barry <azaeres@gmail.com>
2018-05-23 03:26:06 -0700

    Cleanup persistence drivers

--- src/models/localStorageDriver.js
+++ src/models/localStorageDriver.js
@@ -1,25 +1,23 @@
 import { Driver } from 'models/Store';
 
-const initialize = async storeId => {
-  const got = JSON.parse(localStorage.getItem(storeId));
-  // console.log('> : got', got);
-  const initial = got === null ? undefined : got;
-  // console.log('> : initial', initial);
-  return Promise.resolve(initial);
-};
-const getter = async storeId => {
-  // console.log('> get : ', storeId);
-  // const value = cache[storeId];
-  const value = localStorage.getItem(storeId);
-  return Promise.resolve(JSON.parse(value));
-};
-const setter = async (storeId, value) => {
-  // console.log('> set : storeId', storeId);
-  // console.log('> : value', value);
-  // cache[storeId] = value;
-  localStorage.setItem(storeId, JSON.stringify(value));
-  return Promise.resolve();
-};
-
 const localStorageDriver = Driver(initialize, getter, setter);
 export default localStorageDriver;
+
+async function initialize(storeId, initialValue) {
+  const value = JSON.parse(localStorage.getItem(storeId));
+  if (value === null) {
+    return Promise.resolve(initialValue);
+  } else {
+    return Promise.resolve(value);
+  }
+}
+
+async function getter(storeId) {
+  const value = localStorage.getItem(storeId);
+  return Promise.resolve(JSON.parse(value));
+}
+
+async function setter(storeId, value) {
+  localStorage.setItem(storeId, JSON.stringify(value));
+  return Promise.resolve();
+}
--- src/models/localforageDriver.js
+++ src/models/localforageDriver.js
@@ -1,31 +1,24 @@
 import localforage from 'localforage';
 import { Driver } from 'models/Store';
 
-const initialize = async storeId => {
-  // console.log('> initialize: storeId', storeId);
-  const getItemPromise = localforage.getItem(storeId).then(got => {
-    // console.log('> : got', got);
-    const initial = got === null ? undefined : got;
-    // console.log('> : initial', initial);
-    return initial;
-  });
-  return getItemPromise;
-};
-const getter = async storeId => {
-  // console.log('> get : ', storeId);
-  // const value = cache[storeId];
-  return await localforage.getItem(storeId);
-};
-const setter = async (storeId, value) => {
-  // console.log('> set : storeId', storeId);
-  // console.log('> : value', value);
-  // cache[storeId] = value;
-  const result = await localforage.setItem(storeId, value);
-  // console.log('> set : result', result);
-  return result;
-};
-
 const localforageDriver = Driver(initialize, getter, setter);
 export default localforageDriver;
 
 // window.clear = () => localforage.clear();
+
+async function initialize(storeId, initialValue) {
+  const value = await localforage.getItem(storeId);
+  if (value === null) {
+    return initialValue;
+  } else {
+    return value;
+  }
+}
+
+async function getter(storeId) {
+  return await localforage.getItem(storeId);
+}
+
+async function setter(storeId, value) {
+  return await localforage.setItem(storeId, value);
+}
