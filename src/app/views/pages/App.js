3d124cc5c6146771e42ba8e5f5a1673a48425cbd
Ryan Barry <azaeres@gmail.com>
2018-05-27 22:05:14 -0700

    Added app as submodule

--- src/app/views/pages/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'app/views/pages/App.css';
-import { hot } from 'react-hot-loader';
-
-function App({ children }) {
-  return <div className="App">{children}</div>;
-}
-
-export default hot(module)(App);
